ab197d754ecb06457d1bd27ea2a54b24d87ec413
Spati Dube <sdube@appdev.yearup.org>
2025-06-26 14:45:28 -0500

    adding impl to category controller methods

--- capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,5 +1,6 @@
 package org.yearup.controllers;
 
+import java.sql.SQLException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +14,7 @@ import java.util.List;
 // add the annotations to make this a REST controller
 @RestController // this makes the class a rest API controller
 // add the annotation to make this controller the endpoint for the following url: //"http://localhost:8080/categories"
-@RequestMapping("categories")
+@RequestMapping("/categories")
 // add annotation to allow cross site origin requests
 @CrossOrigin // allows cross-origin requests form the front end allowing front end access to access thebackend
 
@@ -37,29 +38,30 @@ public class CategoriesController {
     @GetMapping()
     @PreAuthorize(("permitAll()"))
 
-    public List<Category> getAll() {
+    public List<Category> getAll() throws SQLException {
         // find and return all categories
         return categoryDao.getAllCategories();
     }
 
     // add the appropriate annotation for a get action
-    @GetMapping("{id}")
+    @RequestMapping(path = "/{id}")
     @PreAuthorize("permitAll()")
     public Category getById(@PathVariable int id) {
 
         // get the category by id
-        return getById(id);
+        return categoryDao.getById(id);
 
     }
         // the url to return all products in category 1 would look like this
         // https://localhost:8080/categories/1/products
         @GetMapping("{categoryId}/products")
         @PreAuthorize("permitAll()")
+
         public List<Product> getProductsById( @PathVariable int categoryId)
 
         {
             // get a list of product by categoryId
-            return getProductsById(categoryId);
+            return productDao.listByCategoryId(categoryId);
         }
 
         // add annotation to call this method for a POST action
@@ -69,7 +71,7 @@ public class CategoriesController {
         public Category addCategory(@RequestBody Category category)
         {
             // insert the category
-            return addCategory(category);
+            return categoryDao.create(category);
         }
 
         // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
@@ -80,7 +82,7 @@ public class CategoriesController {
         public void updateCategory ( @PathVariable int id, @RequestBody Category category)
         {
             // update the category by id
-            updateCategory(id, category);
+            categoryDao.update(id, category);
         }
 
 
@@ -92,6 +94,6 @@ public class CategoriesController {
 
         {
             // delete the category by id
-            deleteCategory(id);
+            categoryDao.delete(id);
         }
     }
