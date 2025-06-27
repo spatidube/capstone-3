2ef399fe476de6e156aebcb1e44ec2c802a820db
Spati Dube <sdube@appdev.yearup.org>
2025-06-26 21:44:35 -0500

    fixed update/search bug

--- capstone-starter/src/main/java/org/yearup/controllers/ProductsController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/ProductsController.java
@@ -85,7 +85,8 @@ public class ProductsController
     {
         try
         {
-            productDao.create(product);
+            //fixed update and search bug
+            productDao.update(id, product);
         }
         catch(Exception ex)
         {
